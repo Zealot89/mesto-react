46dfe4c31c8a8ad325a71fb3e63e92db2a6a10fe
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-08-28 22:14:54 +0300

    Updates

---
+++ precache-manifest.00f736e6f2112783effde092d483f9fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aad30b17ca678eff3900efad0231046",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "77a8d42402c4aa71e7d0",
+    "url": "/mesto-react/static/css/main.44cb60f8.chunk.css"
+  },
+  {
+    "revision": "87fe720923abf27d7cd4",
+    "url": "/mesto-react/static/js/2.6e4763c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.6e4763c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a8d42402c4aa71e7d0",
+    "url": "/mesto-react/static/js/main.72d30978.chunk.js"
+  },
+  {
+    "revision": "6e04319a44e5e6859875",
+    "url": "/mesto-react/static/js/runtime-main.36fb0e45.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/mesto-react/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/mesto-react/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/mesto-react/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/mesto-react/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/mesto-react/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/mesto-react/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/mesto-react/static/media/Vector.855a8c98.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2babdd3c2d977bf529d311c837a8d3dd.js"
+  "/mesto-react/precache-manifest.00f736e6f2112783effde092d483f9fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mesto-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
